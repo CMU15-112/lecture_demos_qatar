ff12aa26c84ec5233366a9bb2d3db90080b162ad
Ryan Riley <rriley.cs@gmail.com>
2020-01-16 15:15:24 +0300

    Week 1 Thursday demo files

---
+++ week1/operator-demo.py
@@ -0,0 +1,29 @@
+a = 3
+b = 6
+
+print(a+b)
+print(b-a)
+print(a*b)
+print(b/a)
+print(a/b)
+print(b**a)
+
+print( (b+1) / a)
+print( (b+1) // a)
+print( (b+1) % a)
+
+print(a << 1)
+print(a << 3)
+
+print((b+2) >> 1)
+print((b+2) >> 2)
+
+c = 5
+d = 3
+
+print(c & d)
+print(10 & 14)
+
+print(c | d)
+
+print(c ^ d)
---
+++ week1/userio-demo.py
@@ -0,0 +1,11 @@
+print("Class", end="")
+print("room")
+
+print("Item 1", "Item 2", 123, 123.5)
+
+a = 10
+print("The value of a is:", a)
+
+b = int(input("Give me a number: "))
+print("You entered:", b)
+print("Half of that is:", b/2)
