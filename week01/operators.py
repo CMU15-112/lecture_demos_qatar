e2fb0766eaf75d0d8371b08c2d6a1d60d912d025
Ryan Riley <rriley.cs@gmail.com>
2021-01-24 10:30:18 +0300

    Some demos from Thursday

---
+++ week01/operators.py
@@ -0,0 +1,23 @@
+a = 7
+b = 12
+
+print(a+b)
+print(b-a)
+print(a-b)
+print(a*b)
+print(b / a)
+print(a / b)
+print(a ** b)
+
+print(b // a)
+print(b % a)
+
+c = 5
+d = 6
+e = 3
+
+print(c & d)
+print(c | d)
+print(c ^ d)
+print(e << 1)
+print(e >> 1)
---
+++ week01/userio.py
@@ -0,0 +1,18 @@
+a = 5
+
+print("Hi there")
+
+for i in range(5):
+    print("Quick loop")
+    
+print("The value of a is", a)
+
+print("Hi ", end="")
+print("There")
+print("Bob")
+
+userInput = input("Please enter your name: ")
+print("Hi there", userInput)
+
+userAge = int(input("Please enter your age: "))
+print("Hi there.  In five years, you will be... ", userAge + 5)
