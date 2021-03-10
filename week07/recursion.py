cabb23e3e924a590aa2806393a0196c7be9a6c39
Ryan Riley <rriley.cs@gmail.com>
2021-03-10 10:44:29 +0300

    More recursion examples

--- week07/recursion.py
+++ week07/recursion.py
@@ -39,3 +39,46 @@ print(sumListAgain([1,2,3,4,5,6,7,8,9,10,20,50,100]))
 
 # bigList = list(range(0,100000000))
 # print(sumListAgain(bigList))
+
+# def sumDigits(n):
+#     s = 0
+#     while n > 0:
+#         s += n%10
+#         n //= 10
+#     return s
+
+print("---")
+
+def sumDigits(n):
+    if n == 0:
+        return 0
+    return n%10 + sumDigits(n//10)
+
+print(sumDigits(12345))
+
+print("---")
+
+# Return the sum of the numbers in the range [a,b]
+def rangeSum(a, b):
+    if a > b:
+        return rangeSum(b, a)
+    elif a == b:
+        return a
+    
+    return a + rangeSum(a+1,b)
+
+print(rangeSum(5,6))
+print(rangeSum(7,5))
+
+print("---")
+
+def rangeSumAgain(a, b):
+    if a > b:
+        return rangeSumAgain(b, a)
+    elif a == b:
+        return a
+    splitPoint = (a+b)//2
+    return rangeSumAgain(a,splitPoint) + rangeSumAgain(splitPoint+1,b)
+
+print(rangeSumAgain(5,6))
+print(rangeSumAgain(7,5))
