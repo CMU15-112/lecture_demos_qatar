f85a754a95012255e3aea5d0c487f503f0a6dc36
Ryan Riley <rriley.cs@gmail.com>
2021-04-01 17:28:39 +0300

    Updating the sidescroller

--- week10/lec2_sidescroller.py
+++ week10/lec2_sidescroller.py
@@ -5,29 +5,54 @@ def appStarted(app):
     app.bushR = 50
     
     app.heroR = 20
-    app.heroX = 50
+    app.heroX = 250
     app.heroY = app.groundLevel-app.heroR
+    
+    app.dy = 0
+    app.ddy = 1
+    
+    app.scrollX = 0
+
+def timerFired(app):
+    app.heroY += app.dy
+    app.dy += app.ddy
+    
+    if app.heroY+app.heroR >= app.groundLevel and app.heroX > 0 and app.heroX < 2*app.width:
+        app.dy = 0
+        app.heroY = app.groundLevel-app.heroR
 
 def keyPressed(app, event):
     if event.key == "Right":
-        app.heroX += 5
+        if app.heroX - app.scrollX > 800:
+            app.scrollX += 10
+        app.heroX += 10
     elif event.key == "Left":
-        app.heroX -= 5
+        if app.heroX-app.scrollX < 200:
+            app.scrollX -= 10
+        app.heroX -= 10
+    elif event.key == "Up":
+        app.dy = -15
+    elif event.key == "d":
+        app.scrollX += 25
+    elif event.key == "a":
+        app.scrollX -= 25
+        
+    
 
 def redrawAll(app, canvas):
     # Draw the bushes
-    for bushLeft in range(0, app.width, 2*app.bushR):
-        canvas.create_oval(bushLeft, app.groundLevel-app.bushR,
-                           bushLeft+2*app.bushR, app.groundLevel+app.bushR,
+    for bushLeft in range(0, 2*app.width, 2*app.bushR):
+        canvas.create_oval(bushLeft-app.scrollX, app.groundLevel-app.bushR,
+                           bushLeft+2*app.bushR-app.scrollX, app.groundLevel+app.bushR,
                            fill="green")
 
     # The ground
-    canvas.create_rectangle(0, app.groundLevel, app.width, app.height,
+    canvas.create_rectangle(0-app.scrollX, app.groundLevel, 2*app.width-app.scrollX, app.height,
                             fill="brown")
     
     # The hero
-    canvas.create_oval(app.heroX-app.heroR, app.heroY-app.heroR,
-                       app.heroX+app.heroR, app.heroY+app.heroR,
+    canvas.create_oval(app.heroX-app.heroR-app.scrollX, app.heroY-app.heroR,
+                       app.heroX+app.heroR-app.scrollX, app.heroY+app.heroR,
                        fill = "red")
     
 
