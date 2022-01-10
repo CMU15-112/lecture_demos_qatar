0ae5b780d4772811a080e602285488d067c8478e
Eduardo Feo Flushing <eduardofeo@gmail.com>
2022-01-10 12:29:34 +0300

    demos, W1L1

---
+++ week1/data.py
@@ -0,0 +1,20 @@
+"Hello World"
+1
+2
+1.5
+True
+False
+
+"hello world"
+
+print("Hi 112")
+
+print(15112)
+
+print("Hi", 15, "Bye")
+
+
+
+
+
+
---
+++ week1/input.py
@@ -0,0 +1,8 @@
+input()  # just asks for input, the input is not used at all
+
+
+name = input("What's your name?: ")  # asks for input, stores it in a variable
+
+print("Hello, ", name)
+
+
