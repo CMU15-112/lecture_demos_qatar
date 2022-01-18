f1e72a9fbbe460696d2709daa77bf506c54534ff
Eduardo Feo-Flushing <efeoflus@andrew.cmu.edu>
2022-01-18 14:24:37 +0300

    group task week 2

---
+++ week2/group_task2.py
@@ -0,0 +1,22 @@
+# Write a function to sum all multiples of 3 and 7 between a and b
+
+def sumMultiplesOf3And7Between(a, b):
+    return 42
+
+def testSumMultiplesOf3And7Between():
+    print("Testing sumMultiplesOf3Between...")
+    # Define some test cases here
+    print("passed")
+
+
+testSumMultiplesOf3And7Between()
+
+# Write a function that counts how many zero digits in a number, e.g, countZeros(0) = 1, countZeros(10) = 1
+def countZeros(n):
+    return 42
+
+def testCountZeros():
+    print("Testing countZeros...")
+    # Define some test cases here
+    print("passed")
+testCountZeros()
