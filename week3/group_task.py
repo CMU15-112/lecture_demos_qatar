35712d634716a9303bff9cfa3ff3cc6b3110b941
Eduardo Feo-Flushing <efeoflus@andrew.cmu.edu>
2022-01-23 14:24:57 +0300

    group_task.py

---
+++ week3/group_task.py
@@ -0,0 +1,30 @@
+#Write the function alternatingSum(theList) that takes a list of numbers and returns
+#the alternating sum (where the sign alternates from positive to negative or vice versa).
+#For example, alternatingSum([5,3,8,4]) returns 6 (that is, 5-3+8-4).
+
+def alternatingSum(theList):
+    return 42
+
+def testAlternatingSum():
+    print("Testing alternatingSum...")
+    # Define some test cases here
+    print("passed")
+
+testAlternatingSum()
+
+# Write the function isSorted(theList) that:
+# Return True if theList is a list of numbers and it is in sorted order
+# Return False otherwise.
+# [1, 2, 5, 7] -> True
+# [1, 2, 5, 7, 6] -> False
+# [1,3,'a'] -> False
+
+def isSorted(theList):
+    return False
+
+def testIsSorted():
+    print("Testing isSorted ...")
+    # Define some test cases here
+    print("passed")
+
+testIsSorted()
