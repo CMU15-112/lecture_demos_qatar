30bca822f4313074d1be8f351396dbe80195eee6
Eduardo Feo Flushing <eduardofeo@gmail.com>
2023-04-02 15:05:17 +0300

    added exercise

---
+++ week12/countInRange_exercise.py
@@ -0,0 +1,9 @@
+def countInRange(L, a, b):
+    pass
+
+L = [4,7,9,11,12,13,15,16,17]
+assert(countInRange(L, 4, 7)==0)
+assert(countInRange(L, 4, 12)==3)
+assert(countInRange(L, 4, 20)==8)
+assert(countInRange(L, 1, 3)==0)
+assert(countInRange(L, 12, 14)==1)
