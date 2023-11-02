404c2d624615dc8cbf47fc68a40de6ffdc38f9ea
EduardoFF <efeoflus@andrew.cmu.edu>
2023-11-02 09:21:45 +0300

    week10

---
+++ week10/mergeSorted.py
@@ -0,0 +1,6 @@
+# This problem was solved during the optional recitation
+assert(mergeSorted([1,5,6], [3,4]) == [1,3,4,5,6])
+assert(mergeSorted([7,8], [3,4]) == [3,4,7,8])
+assert(mergeSorted([], [1,6]) == [1,6])
+assert(mergeSorted([], []) == [])
+assert(mergeSorted([8], []) == [8])
