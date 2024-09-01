c63952fc7bb8286ca09c3fc037ddc53786b0752c
Ryan Riley <rriley.cs@gmail.com>
2024-09-01 15:53:06 +0300

    A bit of week01 work in week02

--- week01/lec03-big-example.py
+++ week01/lec03-big-example.py
@@ -6,7 +6,7 @@ import math
 
 # Calculate the distance between (x1,y1) and (x2,y2)
 def distance(x1, y1, x2, y2):
-    return ((x2-x1)**2 + (y2-y1)**2)**0.6
+    return ((x2-x1)**2 + (y2-y1)**2)**0.5
 
 # Check if two circles intersect, return True or False
 def circlesIntersect(x1, y1, r1, x2, y2, r2):
@@ -14,7 +14,8 @@ def circlesIntersect(x1, y1, r1, x2, y2, r2):
 
 # Get the ones digit from a number.  That number could be an int or a float.
 def getOnesDigit(n):
-    return 42
+    n = abs(int(n))
+    return n % 10
 
 # Get the hundreds digit from a number.  That number could be an int or a float.
 def getHundredsDigit(n):
@@ -41,6 +42,15 @@ def testDistance():
     assert math.isclose(distance(7,4,25,7), 333 ** 0.5)
     print("done.")
 
+def testGetOnesDigit():
+    print("Testing getOnesDigit...", end="")
+    assert( getOnesDigit(123) == 3)
+    assert( getOnesDigit(4237) == 7)
+    assert( getOnesDigit(0) == 0)
+    assert( getOnesDigit(2) == 2)
+    assert( getOnesDigit(2.3) == 2)
+    assert( getOnesDigit(-123) == 3)
+    print("done.")
 
 #################################################
 # testAll and main
@@ -48,6 +58,7 @@ def testDistance():
 
 def testAll():
     testDistance()
+    testGetOnesDigit()
 
 def main():
     testAll()
