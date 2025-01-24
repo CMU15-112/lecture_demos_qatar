2315aedd9aeb513b2bacfa08ca32293c56a479e1
EduardoFF <efeoflus@andrew.cmu.edu>
2025-01-24 17:50:59 +0300

    completed leastFrequentLetters

--- week3/leastFreqLetters.py
+++ week3/leastFreqLetters.py
@@ -4,16 +4,30 @@ def leastFrequentLetters(s):
     # Find lowest freq
     lowerS = s.lower()
     minCount = len(s)+1
+    if s == "":
+        return ""
     for c in lowerS:
         if c.isalpha():
+            # we have to check the frequency of its lowercase counterpart
+            # using the lowercase string
             cnt = lowerS.count(c)
             if cnt < minCount:
                 minCount = cnt
     result = ""
     # keep lowest freq
-     for c in s:
+    for c in s:
+        #  check only alphabetical
         if c.isalpha():
+            # we have to check the frequency of its lowercase counterpart
+            # using the lowercase string
             cnt = lowerS.count(c.lower())
-            if cnt < minCount:
-                minCount = cnt
-    
+            if cnt == minCount:
+                result += c
+    return result
+
+
+assert leastFrequentLetters("aaaBbcc") == "Bbcc"
+assert leastFrequentLetters("aAbB") == "aAbB"
+assert leastFrequentLetters("aAbBC") == "C"
+assert leastFrequentLetters("") == ""
+print("passed.")
