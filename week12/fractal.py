b6318d0bd8b7e9750cdaa026c123fdce56403648
EduardoFF <efeoflus@andrew.cmu.edu>
2025-04-13 21:29:50 +0300

    more examples week12

---
+++ week12/fractal.py
@@ -0,0 +1,20 @@
+import basic_graphics
+import random
+
+def drawTSquare(canvas, x, y, width,  depth):
+    if depth == 0:
+        return
+    canvas.create_rectangle(x - width //2,
+                            y - width //2,
+                            x + width//2,
+                            y + width//2, fill=None, width=2)
+    next_width = width // 2
+    drawTSquare(canvas, x - width //2, y - width //2, next_width, depth-1)
+    drawTSquare(canvas, x - width //2, y + width //2, next_width,  depth-1)
+    drawTSquare(canvas, x + width //2, y - width //2, next_width,  depth-1)
+    drawTSquare(canvas, x + width //2, y + width //2, next_width,  depth-1)
+    
+def draw(canvas, width, height):
+   drawTSquare(canvas, width//2, height//2, 300, 1)
+
+basic_graphics.run(width=1000, height=1000)
