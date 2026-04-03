4537b350a6cfaf848adda3d9bb858c49f7a76295
Hend Gedawy <hkg@andrew.cmu.edu>
2026-04-03 09:08:39 +0300

    week11, lect3

--- Week11/SideScroller_OOpyAnim.py
+++ Week11/SideScroller_OOpyAnim.py
@@ -10,7 +10,7 @@ class Cloud(object):
         self.x = x
         self.y = y
         #dx captures speed and direction
-            # the cloud moves (2) pixels/update
+            # the cloud moves (2) pixels/step
             # (-) means it moves to the left
         self.dx = -2 
     
@@ -41,6 +41,9 @@ class Player(object):
         #top-left corner coordinates for the rectangle shape
         self.x = x
         self.y = self.groundY-self.height
+        self.dy = 0
+        self.gravity = 1
+        self.onGround = True
         
     def draw(self):
         # The rectangle is the main shape,
@@ -53,22 +56,81 @@ class Player(object):
         drawLine(self.x + 30, self.y + self.height,
                  self.x + 30, self.y + self.height + 12, lineWidth=4)
         
+    def jump(self):
+        if self.onGround:
+            self.dy = -16
+            self.onGround = False
+            
+    def update(self):
+        if self.onGround == False:
+            self.y += self.dy
+            self.dy+= self.gravity
+            
+            if self.y +self.height >= self.groundY:
+                self.y = self.groundY-self.height
+                self.dy = 0
+                self.onGround = True
+    def getBounds(self):
+        return (self.x, self.y, self.width,self.height)
 
 class Obstacle(object):
     
-    def __init__(self, x, groundY):
+    def __init__(self, x, groundY, scrollSpeed):
         self.width = random.choice([25, 30, 35])
         self.height = random.choice([35, 45, 55])
         self.groundY = groundY
         # top-left corner coordinates of the rectangle
         self.x = x
         self.y = self.groundY - self.height
+        self.dx= -scrollSpeed #speed and direction
         
         
     def draw(self):
         drawRect(self.x, self.y, self.width, self.height, fill='crimson')
         drawLine(self.x, self.y, self.x + self.width, self.y, fill='black')
         
+        
+    def update(self):
+        self.x+=self.dx
+        
+    def getBounds(self):
+        return (self.x, self.y, self.width,self.height)
+        
+class Game(object):
+    
+    @staticmethod
+    def updateObstacles(app):
+        
+        kept=[] #new list stores current obstacles 
+        for o in app.obstacles:
+            o.update()
+            
+            #offscreen
+            if o.x+o.width <=0:
+                app.score+=1
+            else:
+                kept.append(o)
+        
+        app.obstacles= kept
+        
+    @staticmethod
+    def detectCollision(player, obstacle):
+        px,py,pw,ph = player.getBounds()
+        ox, oy, ow,oh =obstacle.getBounds()
+        
+        return (px< ox+ow and
+                px+pw > ox and
+                py < oy+oh and
+                py+ph > oy)
+    
+    @staticmethod
+    def updateDifficulty(app):
+        if app.steps%120 ==0:
+            app.scrollSpeed += 0.15
+        
+        if app.steps%180 == 0 and app.spawnDelay > 25:
+            app.spawDelay -= 1
+
 ############################ CMU_GRAPHICS MAIN CODE ###########################
 
 
@@ -80,12 +142,38 @@ def resetGame(app):
     app.gameOver = False
     app.clouds = [Cloud(120, 80), Cloud(420, 120), Cloud(700, 60)]
     app.player = Player(90, app.groundY)
-    app.obstacles = [Obstacle(350, app.groundY)]
+    app.obstacles = []
+    app.score= 0
+    app.spawnDelay = 45 #steps to wait beforeadding new obstacle
+    app.steps =0
+    app.scrollSpeed = 6
     
+def onKeyPress(app, key):
+    if key in ['up','space']:
+        app.player.jump()
+    elif key in 'rR':
+        resetGame(app)
+        
 #by default this is called 30 times/sec
 def onStep(app):
+    
+    if app.gameOver:
+        return
+    
+    app.steps+=1
+    app.player.update()
+    
     for c in app.clouds:
         c.update()
+        
+    Game.updateObstacles(app)
+    
+    if app.steps%app.spawnDelay==0:
+        app.obstacles.append(Obstacle(app.width, app.groundY, app.scrollSpeed))
+    
+    for o in app.obstacles:
+        if Game.detectCollision(app.player, o):
+            app.gameOver= True
     
 def redrawAll(app):
     # Sky
@@ -95,16 +183,31 @@ def redrawAll(app):
     drawRect(0, app.groundY, app.width, app.height-app.groundY, fill='lightGreen')
     drawLine(0, app.groundY, app.width, app.groundY, lineWidth = 3)
     
+    
+    drawLabel(f'Score: {app.score}', 70, 30, size=24, bold=True)
+    
     #Clouds
     for c in app.clouds:
         c.draw()
-    
-    #Player
-    app.player.draw()
+
     
     #Obstacles
     for o in app.obstacles:
         o.draw()
+        
+    #Player
+    app.player.draw()
+    
+    if not app.gameOver:
+        drawLabel('Press space to jump', app.width - 140, 30, size=18)
+    else:
+        drawRect(170, 110, 460, 140, fill='white', border='black')
+        drawLabel('Game Over!', app.width/2, 150, size=32, bold=True, fill='crimson')
+        drawLabel(f'Final Score: {app.score}', app.width/2, 190, size=22)
+        drawLabel('Press r to restart', app.width/2, 225, size=20)
+
+
+    
     
 def main():
     runApp(width=800, height=400)
