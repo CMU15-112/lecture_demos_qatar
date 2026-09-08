a5a120ef8c103eb88ce4e610e82d8065b9c0033d
Ryan Riley <rriley.cs@gmail.com>
2026-09-08 12:45:23 +0300

    Week 3 lec 2

--- week03/week03.py
+++ week03/week03.py
@@ -51,11 +51,16 @@ print("---")
 # Return the least frequently occuring letters in the string
 # Example:
 # leastFrequentLetters("aDq efQ? FB'daf!!!") == 'be'
-def leastFrequentLetters(s):
+def leastFrequentLetters(s):    
     s = removeNonLettersAndMakeLower(s)
+    
+    # Empty strings have no least frequent character
+    if s == "":
+        return ""
+    
     lowestFreq = frequencyOfLeastOccurringLetter(s)
-    res = getLettersWithFrequency(s, freq)
-    res = alphabetize(res)
+    res = getLettersWithFrequency(s, lowestFreq)
+    #res = alphabetize(res)
     return res
 
 def removeNonLettersAndMakeLower(s):
@@ -76,3 +81,59 @@ def frequencyOfLeastOccurringLetter(s):
     return lowestFreq
 
 assert frequencyOfLeastOccurringLetter("adqefqfbdaf") == 1
+assert frequencyOfLeastOccurringLetter("adqfqfdaf") == 2
+
+def getLettersWithFrequency(s, freq):
+    res = ""
+    for letter in "abcdefghijklmnopqrstuvwxyz":
+        cnt = s.count(letter)
+        if cnt == freq:
+            res += letter
+    return res
+
+assert getLettersWithFrequency("adqefqfbdaf", 1) == "be"
+assert getLettersWithFrequency("adqefqfbdaf", 2) == "adq"
+assert getLettersWithFrequency("adqefqfbdaf", 3) == "f"
+assert getLettersWithFrequency("adqefqfbdaf", 4) == ""
+
+
+assert leastFrequentLetters("aDq efQ? FB'daf!!!") == "be"
+assert leastFrequentLetters("") == ""
+assert leastFrequentLetters("1234") == ""
+
+
+
+print("---")
+
+# Anagrams are strings containing the same letters, but in a potentially
+# different order
+# Not case sensitive.  Only consider non-space characters
+# Example: "dormitory", "Dir   tyr oo m"
+def areAnagrams(s1, s2):
+    
+    s1 = s1.lower().replace(" ", "")
+    s2 = s2.lower().replace(" ", "")
+    
+    if len(s1) != len(s2):
+        return False
+    
+    for c in s1:
+        if s1.count(c) != s2.count(c):
+            return False
+    return True
+
+assert areAnagrams("dormitory","dirtyroom") == True
+assert areAnagrams("dormitory","dirtyrom") == False
+assert areAnagrams("dormitory","dirtyrorm") == False
+assert areAnagrams("dormitory","Dirtyroom") == True
+assert areAnagrams("dorm      itory","di rtyr  oom") == True
+assert areAnagrams("12345!@.","12345!@.") == True
+assert areAnagrams("12345!@.3","1345!@.23") == True
+assert areAnagrams(123, 123) == 
+
+
+
+
+
+
+print("done")
